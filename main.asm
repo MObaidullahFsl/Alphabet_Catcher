1df30e62ada691228694e91232d8b10e2069c3c6
CyberSmeagol <mobaidullahfsl@gmail.com>
2024-11-25 18:28:36 +0500

    second

--- main.asm
+++ main.asm
@@ -16,3 +16,69 @@ mov cx, 17000  ; Number of pixels on screen (320 * 200 = 64000)
 rep stosb          ; Fill the screen with the value in AL (black)
 ret
 
+
+clr2:
+pusha 
+mov ax, 0xA000     ; Set the segment for video memory (Mode 13h)
+mov es, ax         ; ES points to video memory
+xor di, di         ; DI = 0, start at the beginning of video memory
+
+mov al, 0          ; Set the color to black (0, can change to other color)
+
+mov cx, 47000   ; Number of pixels on screen (320 * 200 = 64000)
+rep stosb      
+
+
+popa
+
+ret 
+
+
+delay:
+	pusha
+	pushf
+
+	mov cx, [speed]
+	mydelay:
+	mov bx, 8000     ;; increase this number if you want to add more delay, and decrease this number if you want to reduce delay.
+	mydelay1:
+	dec bx
+	jnz mydelay1
+	loop mydelay
+
+	popf
+	popa
+ret	 
+
+
+
+randG:
+   push bp
+   mov bp, sp
+   pusha
+   cmp word [rand], 0
+   jne next
+
+  MOV     AH, 00h   ; interrupt to get system timer in CX:DX 
+  INT     1AH
+  inc word [rand]
+  mov     [randnum], dx
+  jmp next1
+
+  next:
+  mov     ax, 25173          ; LCG Multiplier
+  mul     word  [randnum]     ; DX:AX = LCG multiplier * seed
+  add     ax, 13849          ; Add LCG increment value
+  ; Modulo 65536, AX = (multiplier*seed+increment) mod 65536
+  mov     [randnum], ax          ; Update seed = return value
+
+ next1:xor dx, dx
+ mov ax, [randnum]
+ mov cx, [bp+4]
+ inc cx
+ div cx
+ 
+ mov [bp+6], dx
+ popa
+ pop bp
+ ret 2
